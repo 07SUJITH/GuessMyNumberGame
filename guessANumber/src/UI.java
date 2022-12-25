d840b841efb1d6810fb96b1fed9257a91e47c125
SUJITH_T_S <99735954+2002SUJITH@users.noreply.github.com>
2022-12-25 11:37:32 +0530

    Add files via upload

--- guessANumber/src/UI.java
+++ guessANumber/src/UI.java
@@ -1,106 +1,111 @@
-import java.awt.*;
-import java.awt.event.*;
+import java.awt.Color;
+import java.awt.Font;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 import javax.swing.SwingConstants;
+import javax.swing.ImageIcon;
 
-public class UI implements ActionListener{
-	JFrame jf;
-	JLabel guessDisplay;
-	JButton clearButton
-	JButton zeroButton;
-	JButton oneButton;
-	JButton twoButton;
-	JButton threeButton;
-	JButton fourButton;
-	JButton enterButton;
-	JButton fiveButton;
-	JButton sixButton;
-	JButton sevenButton;
-	JButton eightButton;
-	JButton nineButton;
-    public UI(){
-        jf = new JFrame("Number Guess");
-        jf.setSize(400,400);
+
+public class UI2{
+    public UI2(){
+		Font myFont = new Font("Ink Free",Font.BOLD,30);
+		Color color =new Color(192,192,192);
+        JFrame jf = new JFrame("Number Guess");
+        jf.setSize(420,420);
         jf.setLayout(null);
         jf.setLocation(0,0);
-
-		guessDisplay = new JLabel("Guess the Number 1 to 100 ");
-		guessDisplay.setBounds(95,20,285,40);
-		guessDisplay.setBackground(Color.black);
-		guessDisplay.setOpaque(true);
-		guessDisplay.setHorizontalAlignment(SwingConstants.RIGHT);
-		guessDisplay.setForeground(Color.white);
-		jf.add(guessDisplay);	
+		ImageIcon image =new ImageIcon("logo.jpg");
+        jf.setIconImage(image.getImage());
+        jf.getContentPane().setBackground(new Color(0,139,139));
 	
-		clearButton = new JButton("C");
-		clearButton.setBounds(30,20,60,40);
-		clearButton.setBackground(Color.gray);
-		jf.add(clearButton);
+	JLabel guessDisplay = new JLabel("Guess the Number 1 to 100 ");
+	guessDisplay.setFont(myFont);
+	guessDisplay.setBounds(95,20,285,40);
+	guessDisplay.setBackground(Color.black);
+	guessDisplay.setOpaque(true);
+	guessDisplay.setHorizontalAlignment(SwingConstants.RIGHT);
+	guessDisplay.setForeground(Color.white);
+	jf.add(guessDisplay);	
 	
-		zeroButton = new JButton("0");
-		zeroButton.setBounds(30,70,80,80);
-		zeroButton.setBackground(Color.gray);
-		jf.add(zeroButton);
+	JButton clearButton = new JButton("C");
+	clearButton.setBounds(30,20,60,40);
+	clearButton.setBackground(color);
+	clearButton.setFont(myFont);
+	jf.add(clearButton);
 	
+	JButton zeroButton = new JButton("0");
+	zeroButton.setBounds(30,70,80,80);
+	zeroButton.setBackground(color);
+	zeroButton.setFont(myFont);
+	jf.add(zeroButton);
 
-		oneButton = new JButton("1");
-		oneButton.setBounds(120,70,80,80);
-		oneButton.setBackground(Color.gray);
-		jf.add(oneButton);
+	JButton oneButton = new JButton("1");
+	oneButton.setBounds(120,70,80,80);
+	oneButton.setBackground(color);
+	oneButton.setFont(myFont);
+	jf.add(oneButton);
 
-		twoButton = new JButton("2");
-		twoButton.setBounds(210,70,80,80);
-		twoButton.setBackground(Color.gray);
-		jf.add(twoButton);
+	JButton twoButton = new JButton("2");
+	twoButton.setBounds(210,70,80,80);
+	twoButton.setBackground(color);
+	twoButton.setFont(myFont);
+	jf.add(twoButton);
 
-		threeButton = new JButton("3");
-		threeButton.setBounds(300,70,80,80);
-		threeButton.setBackground(Color.gray);
-		jf.add(threeButton);
+	JButton threeButton = new JButton("3");
+	threeButton.setBounds(300,70,80,80);
+	threeButton.setBackground(color);
+	threeButton.setFont(myFont);
+	jf.add(threeButton);
 
-		fourButton = new JButton("4");
-   		fourButton.setBounds(30,160,80,80);
-   		fourButton.setBackground(Color.gray);
-   		jf.add(fourButton);
+	JButton fourButton = new JButton("4");
+   	fourButton.setBounds(30,160,80,80);
+   	fourButton.setBackground(color);
+	fourButton.setFont(myFont);
+   	jf.add(fourButton);
  
- 	    enterButton = new JButton("Enter");
- 		enterButton.setBounds(120,160,170,80);
- 		enterButton.setBackground(Color.gray);
- 		jf.add(enterButton);
+ 	JButton enterButton = new JButton("Enter");
+ 	enterButton.setBounds(120,160,170,80);
+ 	enterButton.setBackground(new Color(153,50,204));
+	enterButton.setFont(myFont);
+ 	jf.add(enterButton);
  	
- 		fiveButton = new JButton("5");
-		fiveButton.setBounds(300,160,80,80);
-		fiveButton.setBackground(Color.gray);
-		jf.add(fiveButton);
+ 	JButton fiveButton = new JButton("5");
+	fiveButton.setBounds(300,160,80,80);
+	fiveButton.setBackground(color);
+	fiveButton.setFont(myFont);
+	jf.add(fiveButton);
 	
-		sixButton = new JButton("6");
-		sixButton.setBounds(30,250,80,80);
-		sixButton.setBackground(Color.gray);
-		jf.add(sixButton);
+	JButton sixButton = new JButton("6");
+	sixButton.setBounds(30,250,80,80);
+	sixButton.setBackground(color);
+	sixButton.setFont(myFont);
+	jf.add(sixButton);
 	
-		sevenButton = new JButton("7");
-		sevenButton.setBounds(120,250,80,80);
-    	sevenButton.setBackground(Color.gray);
-    	jf.add(sevenButton);
+	JButton sevenButton = new JButton("7");
+	sevenButton.setBounds(120,250,80,80);
+    sevenButton.setBackground(color);
+	sevenButton.setFont(myFont);
+    jf.add(sevenButton);
 
-    	eightButton = new JButton("8");
-    	eightButton.setBounds(210,250,80,80);
-    	eightButton.setBackground(Color.gray);
-    	jf.add(eightButton);
+    JButton eightButton = new JButton("8");
+    eightButton.setBounds(210,250,80,80);
+    eightButton.setBackground(color);
+	eightButton.setFont(myFont);
+    jf.add(eightButton);
 
-    	nineButton = new JButton("9");
-    	nineButton.setBounds(300,250,80,80);
-    	nineButton.setBackground(Color.gray);
-    	jf.add(nineButton);
+    JButton nineButton = new JButton("9");
+    nineButton.setBounds(300,250,80,80);
+    nineButton.setBackground(color);
+	nineButton.setFont(myFont);
+    jf.add(nineButton);
 
-		jf.setVisible(true);
-		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	jf.setVisible(true);
+	jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	jf.setResizable(false);
     }
-    
     public static void main(String[] args){
-        new UI();
+        new UI2();
     }
-}
 
+}
